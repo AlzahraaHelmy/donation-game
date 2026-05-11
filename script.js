3e1117ecbcac2ba70d420dec66c82c3230310f1d
AlzahraaHelmy <125825678+AlzahraaHelmy@users.noreply.github.com>
2026-05-11 23:16:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function send() {
 
   let amount = Math.floor(Math.random() * 500) + 10;
 
-  fetch("https://script.google.com/macros/s/AKfycbzc96mKHqHWUZIK5ZxiAL5ZCetCQ73_2-UHbQM-_Y1iTojCtuQEXuOHdEqBmhyx6Ckmdw/exec", {
+  fetch("https://script.google.com/macros/s/AKfycbz2Lshx1u6bEvstP2rF6xpHaY0UMfY4jQ8_49H4XyV0Ue2rsG2z-A2Mh-SEjTAAQ-eeZg/exec", {
     method: "POST",
     body: JSON.stringify({
       name: name,
